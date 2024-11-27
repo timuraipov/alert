b1a4e854b666bd31e848e600e851008a74912da6
Timur <timur@MacBook-Pro-Timur.local>
2024-11-27 17:49:14 +0300

    iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/timuraipov/alert/internal/http-server/handlers/metrics"
+	"github.com/timuraipov/alert/internal/storage/inmemory"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	saver, err := inmemory.New()
+	if err != nil {
+		panic(err)
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/", metrics.New(saver))
+	return http.ListenAndServe(":8080", mux)
+}
