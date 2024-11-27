49c968ea5d57b3942631fd4bf9c046af56fd9b62
Timur <timur@MacBook-Pro-Timur.local>
2024-11-27 17:49:46 +0300

    iter1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/timuraipov/alert
+
+go 1.23.1
---
+++ internal/http-server/handlers/metrics/update.go
@@ -0,0 +1,111 @@
+package metrics
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type MetricSaver interface {
+	Save(metric Metric) error
+}
+
+const (
+	MetricTypeGauge   = "gauge"
+	MetricTypeCounter = "counter"
+)
+
+var (
+	ErrMetricNameRequired = errors.New("metric name is required")
+	ErrMetricTypeIsEnum   = errors.New("metric type should be gauge or counter")
+)
+
+type Metric struct {
+	Type         string
+	Name         string
+	ValueCounter int64
+	ValueGauge   float64
+}
+
+func New(saver MetricSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusForbidden)
+			return
+		}
+		fmt.Println("host", r.Host, "path", r.URL.Path)
+		path := r.URL.Path
+		splittedPath := strings.Split(path[1:], "/")
+		metric, err := parseAndValidate(splittedPath)
+		if err != nil {
+			if errors.Is(err, ErrMetricNameRequired) {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = saver.Save(*metric)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	}
+}
+
+// func Update(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method != http.MethodPost {
+// 		w.WriteHeader(http.StatusForbidden)
+// 		return
+// 	}
+// 	fmt.Println("host", r.Host, "path", r.URL.Path)
+// 	path := r.URL.Path
+// 	splittedPath := strings.Split(path[1:], "/")
+// 	err := validate(splittedPath)
+// 	if err != nil {
+// 		if errors.Is(err, ErrMetricNameRequired) {
+// 			w.WriteHeader(http.StatusNotFound)
+// 			return
+// 		}
+// 		w.WriteHeader(http.StatusBadRequest)
+// 		return
+// 	}
+
+// }
+func parseAndValidate(splittedPath []string) (*Metric, error) {
+	if len(splittedPath) != 4 {
+		fmt.Println("len mismatch")
+		return nil, ErrMetricNameRequired
+	}
+	metricType := splittedPath[1]
+	if !(metricType == MetricTypeCounter || metricType == MetricTypeGauge) {
+		fmt.Println("type mismatch")
+
+		return nil, ErrMetricTypeIsEnum
+	}
+	metric := &Metric{
+		Type: metricType,
+	}
+	if metricType == MetricTypeCounter {
+		fmt.Println("counter mismatch")
+
+		value, err := strconv.ParseInt(splittedPath[3], 10, 64)
+		if err != nil {
+			return nil, ErrMetricTypeIsEnum
+		}
+		metric.ValueCounter = value
+	}
+	if metricType == MetricTypeGauge {
+		fmt.Println("Gaugage mismatch")
+
+		value, err := strconv.ParseFloat(splittedPath[3], 64)
+		if err != nil {
+			return nil, ErrMetricTypeIsEnum
+		}
+		metric.ValueGauge = value
+	}
+	metric.Name = splittedPath[2]
+	return metric, nil
+}
