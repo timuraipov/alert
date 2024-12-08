133aa03cd0e0f61ea1ad584c367eebd1416fa598
Timur <timur@MacBook-Pro-Timur.local>
2024-12-08 17:07:46 +0300

    fix addr scheme

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -98,7 +98,7 @@ func (m *MetricsCollector) Run() {
 	}()
 	time.Sleep(time.Duration(m.PollInterval) * time.Second)
 	for {
-		m.Send(m.Addr)
+		m.Send("http://" + m.Addr)
 		time.Sleep(time.Duration(m.ReportCountInterval) * time.Second)
 	}
 
